e86a4dc33adb74e9e99e461b01371fb44b1f38ec
Bruno Leandro Piske <leandropiskebruno@gmail.com>
2016-02-24 08:59:11 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void troca(int *p, int *q);
+/*
+Simples exemplos para usar ponteiros ao passar valores para uma funcao
+*/
+int main(void) {
+	int a = 1;
+	int b = 2;
+	printf("Inverter valores das variaveis a = 1 e b = 2");
+	troca(&a, &b);
+	return 0;
+}
+
+void troca(int *p, int *q) {
+	int temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;
+	printf("Valor a = %d e valor de b = %d", *p, *q);
+}
